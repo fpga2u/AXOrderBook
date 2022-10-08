342bb036465c5f153682392e31925b7b7d121608
fpga2u@5950x <fpga2u@outlook.com>
2022-10-08 08:32:38 +0800

    330M布局布线会失败，250M可以出xclbin。

--- hw/test/hbmArbiter/hbmArbiter_2_2_2_128m/vitis/setting.sh
+++ hw/test/hbmArbiter/hbmArbiter_2_2_2_128m/vitis/setting.sh
@@ -12,7 +12,7 @@ export PLATFORM_REPO_PATHS='/opt/xilinx/platforms'
 export XILINX_PLATFORM='xilinx_u50_gen3x16_xdma_5_202210_1'
 export DEVICE=${PLATFORM_REPO_PATHS}/${XILINX_PLATFORM}/${XILINX_PLATFORM}.xpfm
 export XPART='xcu50-fsvh2104-2L-e'
-export CLKP='330MHz'
+export CLKP='250MHz'
 
 echo "PLATFORM_REPO_PATHS : $PLATFORM_REPO_PATHS"
 echo "XILINX_PLATFORM     : $XILINX_PLATFORM"
